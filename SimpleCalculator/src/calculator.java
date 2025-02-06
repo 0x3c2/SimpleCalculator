e17a0f4c858542d36055017390b115b2f311b473
Ahmad Al-Khateeb <100092845+irbid0x@users.noreply.github.com>
2025-02-06 13:08:29 +0300

    Update calculator.java

--- SimpleCalculator/src/calculator.java
+++ SimpleCalculator/src/calculator.java
@@ -4,29 +4,31 @@ public class calculator
     public static void main(String []args)
     {
         Scanner input=new Scanner(System.in);
-        float x,z;
+        float x, y;
         int oper;
-        System.out.print("1. Sum (+)\n2. Sub (-)\n3. Multi (*)\n4. Div (/)\n5. Mod (%)\nChoose the operation : ");
+        System.out.print("1. Sum (+)\n2. Sub (-)\n3. Multi (*)\n4. Div (/)\n5. Mod (%)\n0. Exit\nChoose the operation : ");
         oper =input.nextInt();
+        if(oper==0) return;
+        
         System.out.println("Enter two numbers to calculate :");
         x=input.nextInt();
-        z=input.nextInt();
+        y =input.nextInt();
         switch(oper)
         {
             case 1:
-                System.out.println(x+"+"+z+" = "+(x+z));
+                System.out.println(x+"+"+y+" = "+(x+y));
                 break;
             case 2:
-                System.out.println(x+"-"+z+" = "+(x-z));
+                System.out.println(x+"-"+y+" = "+(x-y));
                 break;
             case 3:
-                System.out.println(x+"*"+z+" = "+(x*z));
+                System.out.println(x+"*"+y+" = "+(x*y));
                 break;
             case 4:
-                System.out.println(x+"/"+z+" = "+(x/z));
+                System.out.println(x+"/"+y+" = "+(x/y));
                 break;
             case 5:
-                System.out.println(x+"%"+z+" = "+(x%z));
+                System.out.println(x+"%"+y+" = "+(x%y));
                 break;
             default:
                 System.out.println("invalid input");
