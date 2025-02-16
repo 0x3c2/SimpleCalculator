b98981c3f8c616d54de99aa544ef96d5d12c6153
Ahmad Al-Khateeb <100092845+irbid0x@users.noreply.github.com>
2025-02-16 13:15:44 +0300

    Update calculator.java

--- SimpleCalculator/src/calculator.java
+++ SimpleCalculator/src/calculator.java
@@ -1,56 +1,81 @@
 import java.util.Scanner;
 public class calculator
 {
+    static int operation;
+    public static float howManyNumbers()
+    {
+        int size;
+        Scanner input=new Scanner(System.in);
+        System.out.print("How many numbers you like to calculate : ");
+        size=input.nextInt();
+        float nums[]=new float[size];
+        float calcNums =0;
+        if(operation==1)
+        {
+            for (int i = 0; i < size; i++) {
+                System.out.print("Enter your #"+(i+1)+" number : ");
+                nums[i] = input.nextFloat();
+                calcNums += nums[i];
+            }
+        }
+        if (operation==2){
+            for (int i = 0; i < size; i++) {
+                System.out.print("Enter your #"+(i+1)+" number : ");
+                nums[i] = input.nextFloat();
+                calcNums -= nums[i];
+            }
+        }
+        if (operation==3){
+            calcNums =1;
+            for (int i = 0;i < size; i++) {
+                System.out.print("Enter your #"+(i+1)+" number : ");
+                nums[i] = input.nextFloat();
+                calcNums *= nums[i];
+            }
+        }
+        System.out.println("Your result is: "+calcNums+"\n");
+        return calcNums;
+    }
+
     public static void main(String []args)
     {
         Scanner input=new Scanner(System.in);
-        float x,z;
-        int oper;
+        float x, y;
+//        int operation;
+
         do
         {
             System.out.print("1. Sum (+)\n2. Sub (-)\n3. Multi (*)\n4. Div (/)\n5. Mod (%)\n0. Exit\nChoose the operation : ");
-            oper = input.nextInt();
-            switch (oper) {
+            operation = input.nextInt();
+            switch (operation) {
                 case 1:
-                    System.out.print("Enter the first number : ");
-                    x = input.nextInt();
-                    System.out.print("Enter the second number : ");
-                    z = input.nextInt();
-                    System.out.println(x + "+" + z + " = " + (x + z)+"\n");
+                    howManyNumbers();
                     break;
                 case 2:
-                    System.out.print("Enter the first number : ");
-                    x = input.nextInt();
-                    System.out.print("Enter the second number : ");
-                    z = input.nextInt();
-                    System.out.println(x + "-" + z + " = " + (x - z)+"\n");
+                    howManyNumbers();
                     break;
                 case 3:
-                    System.out.print("Enter the first number : ");
-                    x = input.nextInt();
-                    System.out.print("Enter the second number : ");
-                    z = input.nextInt();
-                    System.out.println(x + "*" + z + " = " + (x * z)+"\n");
+                    howManyNumbers();
                     break;
                 case 4:
                     System.out.print("Enter the first number : ");
                     x = input.nextInt();
                     System.out.print("Enter the second number : ");
-                    z = input.nextInt();
-                    System.out.println(x + "/" + z + " = " + (x / z)+"\n");
+                    y = input.nextInt();
+                    System.out.println(x + "/" + y + " = " + (x / y)+"\n");
                     break;
                 case 5:
                     System.out.print("Enter the first number : ");
                     x = input.nextInt();
                     System.out.print("Enter the second number : ");
-                    z = input.nextInt();
-                    System.out.println(x + "%" + z + " = " + (x % z)+"\n");
+                    y = input.nextInt();
+                    System.out.println(x + "%" + y + " = " + (x % y)+"\n");
                     break;
                 case 0:
                     System.out.println("Goodbye"); break;
                 default:
                     System.out.println("invalid input, Try again");
             }
-        }while(oper!=0);
+        }while(operation !=0);
     }
 }
